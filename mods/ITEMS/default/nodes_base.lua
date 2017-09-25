6d520f3dd06086390f2f182c5a210f048c304e75
vlapsley <vaughan.lapsley@gmail.com>
2017-09-25 13:53:26 +1000

    Update snow and ice textures

--- mods/ITEMS/default/nodes_base.lua
+++ mods/ITEMS/default/nodes_base.lua
@@ -287,7 +287,9 @@ minetest.register_node("default:snowblock", {
 
 minetest.register_node("default:ice", {
 	description = "Ice",
+	drawtype = "allfaces_optional",
 	tiles = {"default_ice.png"},
+	use_texture_alpha = true,
 	is_ground_content = false,
 	paramtype = "light",
 	groups = {cracky = 3, puts_out_fire = 1, cools_lava = 1},
